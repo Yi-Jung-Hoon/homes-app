9e21e2b461b644f5fe161f5d80efa9a909a211d2
qrisma <user.name>
2024-03-28 12:28:45 +0900

    Services in Angular - 서비스 생성 - 서비스 주입을 통한 상세 화면 구성하기

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,18 +1,43 @@
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   imports: [],
-  template: ` <p>details works! {{housingLocationId}}</p> `,
+  template: ` 
+  <article>
+    <img class="listing-photo" [src]="housingLocation?.photo" alt="Exterior photo of {{ housingLocation?.name }}" />
+    <section class="listing=description">
+      <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+      <p class="listing-location">{{ housingLocation?.city }}, {{ housingLocation?.state }}</p>
+    </section>
+    <section class="listing-features">
+      <h2 class="section-heading">About this housing location</h2>
+      <ul>
+        <li>Units available: {{ housingLocation?.availableUnits }}</li>
+        <li>Does this location have wifi:{{ housingLocation?.wifi }}</li>
+        <li>Does this location have laundry:{{ housingLocation?.laundry }}</li>
+      </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <button class="primary" type="button">Apply now</button>
+    </section>
+  </article>`,
   styleUrl: './details.component.css',
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    // 서비스를 통하여 정보 조회
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
 }
---
+++ src/app/housing.service.ts
@@ -0,0 +1,123 @@
+// Angular의 의존성 주입 시스템에 의해 인스턴스화 될 수 있음을 나타냄
+import { Injectable } from '@angular/core';
+import { HousingLocation } from './housinglocation';
+
+// Injectable 데코레이터를 사용하여 이 클래스가 서비스로 사용될 수 있음을 Angular에 알립니다.
+// providedIn: 'root'는 이 서비스가 애플리케이션의 root injector에 등록되어
+// 애플리케이션 전체에서 사용할 수 있음을 나타냅니다.
+@Injectable({ providedIn: 'root' })
+export class HousingService {
+  protected housingLocationList: HousingLocation[] = [
+    // 각 객체는 주택 위치의 상세 정보를 나타냅니다.
+    {
+      id: 0,
+      name: 'Acme Fresh Start Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg',
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 1,
+      name: 'A113 Transitional Housing',
+      city: 'Santa Monica',
+      state: 'CA',
+      photo: '/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg',
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      id: 2,
+      name: 'Warm Beds Housing Support',
+      city: 'Juneau',
+      state: 'AK',
+      photo: '/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg',
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 3,
+      name: 'Homesteady Housing',
+      city: 'Chicago',
+      state: 'IL',
+      photo: '/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 4,
+      name: 'Happy Homes Group',
+      city: 'Gary',
+      state: 'IN',
+      photo: '/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg',
+      availableUnits: 1,
+      wifi: true,
+      laundry: false,
+    },
+    {
+      id: 5,
+      name: 'Hopeful Apartment Group',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 6,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg',
+      availableUnits: 5,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 7,
+      name: 'Hopeful Housing Solutions',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/r-architecture-GGupkreKwxA-unsplash.jpg',
+      availableUnits: 2,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      id: 8,
+      name: 'Seriously Safe Towns',
+      city: 'Oakland',
+      state: 'CA',
+      photo: '/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg',
+      availableUnits: 10,
+      wifi: false,
+      laundry: false,
+    },
+    {
+      id: 9,
+      name: 'Capital Safe Towns',
+      city: 'Portland',
+      state: 'OR',
+      photo: '/assets/webaliser-_TPTXZd9mOo-unsplash.jpg',
+      availableUnits: 6,
+      wifi: true,
+      laundry: true,
+    },
+  ];
+
+  constructor() {}
+  getAllHousingLocations(): HousingLocation[] {
+    return this.housingLocationList;
+  }
+  getHousingLocationById(id: number): HousingLocation | undefined {
+    return this.housingLocationList.find(
+      (housingLocation) => housingLocation.id === id
+    );
+  }
+}
